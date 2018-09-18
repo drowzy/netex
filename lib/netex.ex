4dc2103ca04d42737d9954191c96762f1425cf9a
Simon Thörnqvist <simon@plug-n-trade.com>
2018-09-18 08:12:26 +0200

    init project

---
+++ lib/netex.ex
@@ -0,0 +1,18 @@
+defmodule Netex do
+  @moduledoc """
+  Documentation for Netex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Netex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/netex_test.exs
@@ -0,0 +1,8 @@
+defmodule NetexTest do
+  use ExUnit.Case
+  doctest Netex
+
+  test "greets the world" do
+    assert Netex.hello() == :world
+  end
+end
