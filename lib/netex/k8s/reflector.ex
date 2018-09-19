58c53a974083bac258cef9f6d3a504d9bfb04e7a
Simon Thörnqvist <simon@plug-n-trade.com>
2018-09-19 09:38:34 +0200

    add k8s reflector

---
+++ lib/netex/k8s/reflector.ex
@@ -0,0 +1,66 @@
+defmodule Netex.K8s.Reflector do
+  use GenServer
+  require Logger
+
+  alias Kazan.Models.Apimachinery.Meta.V1.WatchEvent
+
+  defmodule State do
+    defstruct conn: nil, controller: nil, params: nil, watcher_pid: nil
+  end
+
+  def start_link(controller, conn, opts) do
+    params = Keyword.get(opts, :params, [])
+
+    GenServer.start_link(__MODULE__, {controller, conn, params}, opts)
+  end
+
+  def init({controller, conn, params}) do
+    {:ok,
+     %State{
+       controller: controller,
+       conn: conn,
+       params: params,
+       watcher_pid: nil
+     }, {:continue, :ok}}
+  end
+
+  def handle_continue(_, state) do
+    {:noreply, do_sync(state)}
+  end
+
+  def handle_cast(:sync, %State{} = state) do
+    {:noreply, state}
+  end
+
+  def handle_info(
+        %WatchEvent{object: object, type: type} = event,
+        %State{controller: ctrl} = state
+      ) do
+    Logger.info("#{__MODULE__} :: Received event with type #{inspect(type)} #{inspect(object)}")
+
+    type =
+      type
+      |> String.downcase()
+      |> String.to_atom()
+
+    _ = process_event(type, ctrl, event)
+    {:noreply, state}
+  end
+
+  defp do_sync(%State{conn: conn, controller: ctrl, params: params} = state) do
+    case Netex.K8s.Client.list_and_watch(conn, &ctrl.list_fn/1, &ctrl.watch_fn/1, params) do
+      {:ok, resource, pid} ->
+        ctrl.handle_sync(resource)
+        Logger.info("#{__MODULE__} :: SYNC Ok! Resource: #{inspect(resource)}")
+        %{state | watcher_pid: pid}
+
+      err ->
+        Logger.error("#{__MODULE__} :: SYNC Failed Error: #{inspect(err)}")
+        state
+    end
+  end
+
+  defp process_event(:added, controller, event), do: controller.handle_added(event)
+  defp process_event(:modified, controller, event), do: controller.handle_modified(event)
+  defp process_event(:deleted, controller, event), do: controller.handle_deleted(event)
+end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Netex.MixProject do
     [
       app: :netex,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,6 +21,7 @@ defmodule Netex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:kazan, "~> 0.9"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
