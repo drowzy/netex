ef3c4a13f312eb65a51ddb56f97e06e7df4a52b2
Simon Thörnqvist <simon@plug-n-trade.com>
2018-09-19 09:38:58 +0200

    controller behaviour

---
+++ lib/netex/controller.ex
@@ -0,0 +1,31 @@
+defmodule Netex.Controller do
+  alias Kazan.Models.Apimachinery.Meta.V1.WatchEvent
+
+  @callback watch_fn(config :: Keyword.t()) :: (any -> any)
+  @callback list_fn(config :: Keyword.t()) :: (any -> any)
+
+  @callback handle_sync(any) :: {:ok, any} | {:error, term}
+
+  @callback handle_added(WatchEvent.t()) :: {:ok, any} | {:error, term}
+  @callback handle_modified(WatchEvent.t()) :: {:ok, any} | {:error, term}
+  @callback handle_deleted(WatchEvent.t()) :: {:ok, any} | {:error, term}
+
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts] do
+      @behaviour Netex.Controller
+
+      def start_link(opts) do
+        %Kazan.Server{} = conn = Keyword.fetch!(opts, :conn)
+        Netex.K8s.Reflector.start_link(__MODULE__, conn, opts)
+      end
+
+      def stop(server) do
+        GenServer.stop(server)
+      end
+
+      def sync_now(server) do
+        GenServer.cast(server, :sync)
+      end
+    end
+  end
+end
